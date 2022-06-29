ceda7832e3b424adc450291c43cb7cec16af9d5b
linuxlite <eze.rey92@gmail.com>
2022-06-29 13:34:49 -0300

    thema chacrakra ui implementado

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme/theme";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-
 export default MyApp;
---
+++ theme/theme.js
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+const theme = extendTheme({
+  colors: {
+    //Color verde claro
+    100: "#5ED7C6",
+  },
+});
+
+export default theme;
