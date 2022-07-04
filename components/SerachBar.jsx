077a47066b2eadbab850df71a40f56677c24c83f
linuxlite <eze.rey92@gmail.com>
2022-07-04 15:52:00 -0300

    ratio solucionado

--- components/SerachBar.jsx
+++ components/SerachBar.jsx
@@ -6,79 +6,81 @@ import { FiSearch } from "react-icons/fi";
 import { Radio, RadioGroup } from "@chakra-ui/react";
 
 export default function SerachBar() {
-  const {
-    searchInput,
-    search,
-    setSearch,
-    serarchCars,
-    ratioValue,
-    setRatioValue,
-  } = useContext(GlobalContext);
-  return (
-    <>
-      <RadioInput
-        ratioValue={ratioValue}
-        setRatioValue={setRatioValue}
-      ></RadioInput>
-      <Stack
-        spacing={2}
-        direction="row"
-        align="center"
-        justifyContent={"center"}
-        width={"full"}
-      >
-        <Input
-          w={"50rem"}
-          color="teal"
-          fontWeight={"bold"}
-          value={search}
-          onChange={(e) => searchInput(e)}
-          placeholder="Search..."
-          size="lg"
-        />
-        <Button
-          rightIcon={<FiSearch w={5} h={5} />}
-          display={{ base: "flex", md: "inline-flex" }}
-          flexFlow={{ base: "column", md: "row" }}
-          height={{ base: "3rem", md: "3rem" }}
-          fontWeight={600}
-          color={"white"}
-          fontSize={{ base: "small", md: "large" }}
-          bg={100}
-          href={"#"}
-          onClick={() => serarchCars(search)}
-          _hover={{
-            bg: 200,
-            transform: "translateY(5px)",
-            transition: " all 1s ease-out",
-          }}
-        >
-          Search
-        </Button>
-      </Stack>
-    </>
-  );
+	const {
+		searchInput,
+		search,
+		setSearch,
+		serarchCars,
+		ratioValue,
+		setRatioValue,
+	} = useContext(GlobalContext);
+	return (
+		<>
+			<RadioInput
+				ratioValue={ratioValue}
+				setRatioValue={setRatioValue}
+			></RadioInput>
+			<Stack
+				spacing={2}
+				direction="row"
+				align="center"
+				justifyContent={"center"}
+				width={"full"}
+			>
+				<Input
+					w={"50rem"}
+					color="teal"
+					fontWeight={"bold"}
+					value={search}
+					onChange={(e) => searchInput(e)}
+					placeholder="Search..."
+					size="lg"
+				/>
+				<Button
+					rightIcon={<FiSearch w={5} h={5} />}
+					display={{ base: "flex", md: "inline-flex" }}
+					flexFlow={{ base: "column", md: "row" }}
+					height={{ base: "3rem", md: "3rem" }}
+					fontWeight={600}
+					color={"white"}
+					fontSize={{ base: "small", md: "large" }}
+					bg={100}
+					href={"#"}
+					onClick={() => serarchCars(search)}
+					_hover={{
+						bg: 200,
+						transform: "translateY(5px)",
+						transition: " all 1s ease-out",
+					}}
+				>
+					Search
+				</Button>
+			</Stack>
+		</>
+	);
 }
 function RadioInput({ ratioValue, setRatioValue }) {
-  const { RatioChangeOnClick } = useContext(GlobalContext);
-  return (
-    <RadioGroup p={"1rem"} onChange={setRatioValue} value={ratioValue}>
-      <Stack direction="row">
-        <Radio
-          onClick={() => RatioChangeOnClick()}
-          colorScheme={"teal"}
-          value="-1"
-        >
-          More expensive
-        </Radio>
-        <Radio
-          onClick={() => RatioChangeOnClick()}
-          colorScheme={"teal"}
-          value="1"
-        >
-          Cheaper
-        </Radio>
-      </Stack>
-    </RadioGroup>
-  );
+	var { searchInput, search, setSearch, serarchCars, serarchCarsRatio } =
+		useContext(GlobalContext);
+	console.log(ratioValue);
+	return (
+		<RadioGroup p={"1rem"} onChange={setRatioValue} value={ratioValue}>
+			<Stack direction="row">
+				<Radio
+					onClick={() => serarchCarsRatio(search,"-1")}
+					colorScheme={"teal"}
+					value="-1"
+				>
+					More expensive
+				</Radio>
+				<Radio
+					onClick={() => serarchCarsRatio(search,"1")}
+					colorScheme={"teal"}
+					value="1"
+				>
+					Cheaper
+				</Radio>
+			</Stack>
+		</RadioGroup>
+	);
 }
--- providers/GlobalContextProvider.jsx
+++ providers/GlobalContextProvider.jsx
@@ -2,90 +2,91 @@ import { React, useState, useEffect } from "react";
 import GlobalContext from "../context/GlobalContext";
 import { useRouter } from "next/router";
 import {
-  getAllCarsFetch,
-  carsSearchFetch,
-  fetchCarDelete,
+	getAllCarsFetch,
+	carsSearchFetch,
+	fetchCarDelete,
 } from "../constantes/constantes";
 
 export default function GlobalContextProvider({ children }) {
-  //OBTENGO LA RUTA
-  const router = useRouter();
-  const hola = "hola mundo";
-  //MODAL ESTADO
-  const [modalContent, setModalContent] = useState("");
+	//OBTENGO LA RUTA
+	const router = useRouter();
+	const hola = "hola mundo";
+	//MODAL ESTADO
+	const [modalContent, setModalContent] = useState("");
 
-  //RATIO SEARCH BAR
-  const [ratioValue, setRatioValue] = useState("1");
+	//RATIO SEARCH BAR
+	const [ratioValue, setRatioValue] = useState("1");
 
-  //ESTADO DE BUSQUEDA
-  const [search, setSearch] = useState("");
-  const [car, setCar] = useState([{}]);
-  //Activo el ratio a todos los cars
-  const RatioChangeOnClick = () => {
-    getAllCarsFetch(ratioValue).then((x) => setCar(x));
-  };
-  //DELETE CAR
-  const deleteCar = (_id) => {
-    fetchCarDelete(_id);
-    getAllCarsFetch(ratioValue).then((x) => setCar(x));
-    setModalContent("Car successfully removed");
-    setTimeout(() => {
-      setModalContent("");
-    }, 1500);
-  };
+	//ESTADO DE BUSQUEDA
+	const [search, setSearch] = useState("");
+	const [car, setCar] = useState([{}]);
 
-  //GET ALL CARS
-  const getAllCars = () => {
-    getAllCarsFetch(ratioValue).then((x) => setCar(x));
-  };
-  //SEARCH CARS
-  const serarchCars = (query) => {
-    carsSearchFetch(query, ratioValue).then((x) => setCar(x));
-  };
-  //FUNCION PARA BUSCAR CAR OBTENER EL VALOR DE BUSQUEDA
-  const searchInput = ({ target }) => {
-    const { value: mySearch } = target;
-    setSearch(mySearch);
-    if (!mySearch) {
-      getAllCarsFetch(ratioValue).then((x) => setCar(x));
-    }
-  };
+	//DELETE CAR
+	const deleteCar = (_id) => {
+		fetchCarDelete(_id);
+		getAllCarsFetch(ratioValue).then((x) => setCar(x));
+		setModalContent("Car successfully removed");
+		setTimeout(() => {
+			setModalContent("");
+		}, 1500);
+	};
 
-  //Obtengo el id de la imagen para ir ha IndividualCar
-  const individualCar = (id) => {
-    const { push } = router;
-    push(`individualCar/${id}`);
-    console.log(id);
-  };
-  //Obtengo el id de la imagen para ir ha IndividualCar
-  const carEdit = (id) => {
-    const { push } = router;
-    push(`carEdit/${id}`);
-    console.log(id);
-  };
+	//GET ALL CARS
+	const getAllCars = () => {
+		getAllCarsFetch(ratioValue).then((x) => setCar(x));
+	};
+	//SEARCH CARS RATIO
+	const serarchCarsRatio = (query, r) => {
+		carsSearchFetch(query, r).then((x) => setCar(x));
+	};
+	//SEARCH CARS
+	const serarchCars = (query) => {
+		carsSearchFetch(query, ratioValue).then((x) => setCar(x));
+	};
+	//FUNCION PARA BUSCAR CAR OBTENER EL VALOR DE BUSQUEDA
+	const searchInput = ({ target }) => {
+		const { value: mySearch } = target;
+		setSearch(mySearch);
+		if (!mySearch) {
+			getAllCarsFetch(ratioValue).then((x) => setCar(x));
+		}
+	};
 
-  return (
-    <GlobalContext.Provider
-      value={{
-        RatioChangeOnClick,
-        carEdit,
-        individualCar,
-        ratioValue,
-        setRatioValue,
-        modalContent,
-        setModalContent,
-        deleteCar,
-        hola,
-        searchInput,
-        search,
-        setSearch,
-        getAllCars,
-        car,
-        setCar,
-        serarchCars,
-      }}
-    >
-      {children}
-    </GlobalContext.Provider>
-  );
+	//Obtengo el id de la imagen para ir ha IndividualCar
+	const individualCar = (id) => {
+		const { push } = router;
+		push(`individualCar/${id}`);
+		console.log(id);
+	};
+	//Obtengo el id de la imagen para ir ha IndividualCar
+	const carEdit = (id) => {
+		const { push } = router;
+		push(`carEdit/${id}`);
+		console.log(id);
+	};
+
+	return (
+		<GlobalContext.Provider
+			value={{
+				serarchCarsRatio,
+				carEdit,
+				individualCar,
+				ratioValue,
+				setRatioValue,
+				modalContent,
+				setModalContent,
+				deleteCar,
+				hola,
+				searchInput,
+				search,
+				setSearch,
+				getAllCars,
+				car,
+				setCar,
+				serarchCars,
+			}}
+		>
+			{children}
+		</GlobalContext.Provider>
+	);
 }
